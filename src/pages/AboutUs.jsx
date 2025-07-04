ff4532f878356e4a54d7324279bb298c8dad25b1
Abdullah0004439 <ahmadabdullah@cosmoinc.co>
2025-07-04 17:58:46 +0500

    aboutushero

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,11 +1,14 @@
-import GrowYourBusinessSection from "@/components/PageComponents/Spaces/Desktop/GrowYourBusinessSection";
+import AboutUsHero from "@/components/PageComponents/AboutUs/Desktop/AboutUsHero";
+import AboutUsMission from "@/components/PageComponents/AboutUs/Desktop/AboutUsMission";
+import { cardsData } from "@/constants/AboutUsMission";
 import React from "react";
 
 function AboutUs() {
   return (
     <div>
       <div className="max-md:hidden">
-        <GrowYourBusinessSection />
+        <AboutUsHero/>
+        <AboutUsMission cardsData={cardsData} />
       </div>
       <div className="md:hidden">Home Mobile</div>
     </div>
