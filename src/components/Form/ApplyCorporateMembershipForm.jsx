a46d7f92d4a51e8578600b25dc8504f26b778b88
Abdullah0004439 <ahmadabdullah@cosmoinc.co>
2025-07-15 19:35:52 +0500

    forms

--- src/components/Form/ApplyCorporateMembershipForm.jsx
+++ src/components/Form/ApplyCorporateMembershipForm.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import locationImg from "../../assets/images/form/spaces-form.webp";
 import { ArrowLeft } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+
 
 function ApplyCorporateMembershipForm({ onBack, onSubmit }) {
+   const navigate = useNavigate();
   const [form, setForm] = useState({
     firstName: "",
     lastName: "",
@@ -46,7 +49,7 @@ function ApplyCorporateMembershipForm({ onBack, onSubmit }) {
   };
 
   return (
-    <div className="flex gap-12 p-6 flex-row max-w-[1280px] mx-auto justify-center items-center min-h-screen">
+    <div className="flex gap-12 p-6 flex-row max-w-[1280px] mx-auto items-center min-h-screen">
       {/* Left Image */}
       <div className="w-full max-w-[40%] flex-shrink-0 flex">
         <div className="rounded-[8px] max-w-[500px] overflow-hidden bg-white">
@@ -58,21 +61,26 @@ function ApplyCorporateMembershipForm({ onBack, onSubmit }) {
         </div>
       </div>
       {/* Right Form */}
-      <div className="flex flex-col items-center w-full max-w-[60%]">
-        <button
-          className="flex items-center gap-2 text-[#222] text-[15px] mb-4 hover:underline"
-          onClick={onBack}
-        >
-          <ArrowLeft size={20} /> Back
-        </button>
-        <div className="w-full max-w-[420px] bg-white rounded-[10px] border p-6">
-          <h2 className="text-[20px] md:text-[22px] font-[700] text-center mb-2">
+      <div className="flex flex-col items-stretch w-full max-w-[60%]">
+         <button
+                  className="flex items-center pl-10 pt-15 gap-2 text-[#222] text-[15px] mb-4 hover:underline"
+                   onClick={() => navigate(-1)}
+                >
+                  <ArrowLeft size={20} /> Back
+                </button>
+        <div className="w-full max-w-[420px] bg-white rounded-[10px]  p-6">
+          <div className="w-[537px]">
+          <h3 className="text-[20px] md:text-[22px] font-[700] text-center mb-2 ">
             APPLY FOR A CORPORATE MEMBERSHIP
-          </h2>
-          <p className="text-[15px] font-[400] text-center mb-5 text-[#222]">
+          </h3>
+          </div>
+          <div className="w-[537px]">
+          <h4 className="text-[15px] font-[400] text-center mb-5 text-[#222] ">
             Fill out the form below to start the process of bringing Evolve to
             your workplace! We'll be in touch shortly.
-          </p>
+          </h4>
+          </div>
+          <div className="w-[537px]  self-stretch gap-[12px] p-[32px_24px] rounded-[10px] border border-[#D4D4D4] bg-[#FCFCFC]">
           <form
             className="flex flex-col gap-3"
             onSubmit={handleSubmit}
@@ -219,10 +227,13 @@ function ApplyCorporateMembershipForm({ onBack, onSubmit }) {
                 className="mt-1 px-2 h-[40px] border border-[#D4D4D4] rounded-[4px] bg-[#FFFFFF] focus:border-[#4AB04A] focus:outline-none w-full placeholder:text-[#6F6D66] placeholder:text-[12px] !placeholder:font-[400] resize-vertical min-h-[60px]"
               />
             </label>
+            <Link to = "/application-submitted">
             <button type="submit" className="mt-2 btnPrimary w-full">
               SUBMIT NOW
             </button>
+            </Link>
           </form>
+          </div>
         </div>
       </div>
     </div>
