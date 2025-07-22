5a1ceb9a16cc062fe870ca81c7e5d29aac131f16
nomanqadir-cosmoinc <nomanqadir@cosmoinc.co>
2025-07-22 18:05:42 +0500

    membership benefits page complete

--- src/components/PageComponents/MembershipBenefits/Desktop/FreeAssessmentForMembership.jsx
+++ src/components/PageComponents/MembershipBenefits/Desktop/FreeAssessmentForMembership.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function FreeAssessmentForMembership() {
   return (
     <div className="relative w-full MembershipBenefitsFreeAssessment">
-      <div className="w-full max-w-[1280px] px-8 py-16 mx-auto z-10 h-full items-center flex flex-row relative">
+      <div className="w-full max-w-[1280px] px-4 md:px-8 py-16 mx-auto z-10 h-full items-end md:items-center flex flex-row relative">
         <div className="bg-[#ffffff] rounded-[10px] shadow-lg p-8 max-w-[346px]">
           <h3 className="text-[#000] uppercase leading-[26px] !font-[700]">
             Start Strong with a Free Personalised Assessment
--- src/components/PageComponents/MembershipBenefits/Desktop/TrainWithOutWait.jsx
+++ src/components/PageComponents/MembershipBenefits/Desktop/TrainWithOutWait.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function TrainWithOutWait() {
   return (
     <div className="w-full py-12">
-      <div className="w-full max-w-[1280px] px-8 mx-auto flex flex-row items-center justify-between">
+      <div className="w-full max-w-[1280px] px-4 md:px-8 mx-auto flex gap-4 md:gap-0 flex-col md:flex-row items-start md:items-center justify-between">
         <div className="max-w-[360px]">
           <h2 className="text-[#000] uppercase !font-[700] leading-[26px]">
             Train Your Way
@@ -12,7 +12,7 @@ function TrainWithOutWait() {
           </h2>
         </div>
 
-        <div className="max-w-[683px] text-[#000] description leading-[26px]">
+        <div className="w-full md:max-w-[683px] text-[#000] description leading-[24px] md:leading-[26px]">
           <p>
             With Evolve, your membership unlocks affordable fitness options
             tailored to your lifestyle, offering more space and variety than
