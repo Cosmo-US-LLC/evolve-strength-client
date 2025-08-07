6515a9370f8153549e05174ef4e7bec2b4d80eb2
nomanqadir-cosmoinc <nomanqadir@cosmoinc.co>
2025-08-07 14:44:45 +0500

    Add GTM and franchise Video and home page 2nd section on mobile height reduce

--- src/components/PageComponents/Franchise/Desktop/FranchiseHero.jsx
+++ src/components/PageComponents/Franchise/Desktop/FranchiseHero.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import GrowYourBusinessSectionImage from "@/assets/images/spaces/GrowYourBusinessSection/GrowYourBusinessSectionImage.webp";
 
 function FranchiseHero() {
   return (
     <div className="w-full pb-12 pt-[90px] md:pt-[120px]">
-      <div className="w-full max-w-[1280px] px-4 md:px-8 mx-auto flex flex-col gap-12">
+      <div className="w-full max-w-[1280px] px-4 md:px-8 mx-auto flex flex-col gap-8">
         <div className="flex md:flex-row flex-col gap-4 md:gap-10">
           <div className="md:w-[50%] w-full flex ">
             <h1 className="max-w-[655px] text-left leading-[50px] md:leading-[56px] uppercase">
@@ -14,7 +13,7 @@ function FranchiseHero() {
           </div>
           <div className="flex flex-col md:w-[50%] w-full text-left justify-center items-start gap-5">
             <h3 className="text-[#000] leading-[26px] font-[400] md:w-[585px]">
-              Canada’s fastest-growing fitness facility is now franchising.
+              Canada's fastest-growing fitness facility is now franchising.
               Leverage our proven multi-revenue model combining fitness,
               healthcare, and real estate.
             </h3>
@@ -23,8 +22,21 @@ function FranchiseHero() {
             </Link>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center ">
-          <img src={GrowYourBusinessSectionImage} alt="Description" />
+        <div className="flex flex-col items-center justify-center">
+          <div className="w-full max-w-[1200px]">
+            <video
+              className="w-full h-auto md:h-[483px] rounded-lg shadow-lg object-cover"
+              autoPlay
+              muted
+              loop
+              playsInline
+              controls={false}
+            >
+              <source src="/videos/FranchiseHero.webm" type="video/webm" />
+              <source src="/videos/FranchiseHero.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
         </div>
       </div>
     </div>
