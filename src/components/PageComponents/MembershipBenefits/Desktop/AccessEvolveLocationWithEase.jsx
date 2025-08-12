24d73f4b8b105c11cecf95058498ec1edce71425
Abdullah0004439 <ahmadabdullah@cosmoinc.co>
2025-08-12 13:30:34 +0500

    Location And benefits slider

--- src/components/PageComponents/MembershipBenefits/Desktop/AccessEvolveLocationWithEase.jsx
+++ src/components/PageComponents/MembershipBenefits/Desktop/AccessEvolveLocationWithEase.jsx
@@ -10,12 +10,51 @@ import LeftArrowIcon from "@/assets/images/MembershipBenefits/AccessEvolveLocati
 import RightArrowIcon from "@/assets/images/MembershipBenefits/AccessEvolveLocationWithEase/right-arrow.svg";
 
 import Slide1 from "@/assets/images/MembershipBenefits/AccessEvolveLocationWithEase/slide1.webp";
+import Slide2 from "/src/assets/images/home/facility/seton5.webp";
+import Slide3 from "/src/assets/images/home/facility/seton6.webp";
+import Slide4 from "/src/assets/images/home/facility/seton7.webp";
+import Slide5 from "/src/assets/images/home/facility/north4.webp";
+import Slide6 from "/src/assets/images/home/facility/north5.webp";
+import Slide7 from "/src/assets/images/home/facility/downtown_3.webp";
+import Slide8 from "/src/assets/images/home/facility/downtown_4.webp";
+import Slide9 from "/src/assets/images/home/facility/downtown_5.webp";
+import Slide10 from "/src/assets/images/home/facility/royal_oak_5.webp";
+import Slide11 from "/src/assets/images/home/facility/royal_oak_6.webp";
+import Slide12 from "/src/assets/images/home/facility/royal_oak_8.webp";
+import Slide13 from "/src/assets/images/home/facility/south_3.webp";
+import Slide14 from "/src/assets/images/home/facility/south_8.webp";
+import Slide15 from "/src/assets/images/home/facility/sunridge3.webp";
+import Slide16 from "/src/assets/images/home/facility/sunridge5.webp";
+import Slide17 from "/src/assets/images/home/facility/sunridge7.webp";
+import Slide18 from "/src/assets/images/home/facility/brentwood3.webp";
+import Slide19 from "/src/assets/images/home/facility/brentwood6.webp";
+import Slide20 from "/src/assets/images/home/facility/brentwood8.webp";
 
 const AccessEvolveLocationWithEase = () => {
   const slides = [
-    { imageUrl: Slide1, alt: "Evolve gym location 1" },
-    { imageUrl: Slide1, alt: "Evolve gym location 2" },
-    { imageUrl: Slide1, alt: "Evolve gym location 3" },
+   
+  { imageUrl: Slide1, alt: "Evolve gym location 1" },
+  { imageUrl: Slide2, alt: "Evolve gym location 2" },
+  { imageUrl: Slide3, alt: "Evolve gym location 3" },
+  { imageUrl: Slide4, alt: "Evolve gym location 4" },
+  { imageUrl: Slide5, alt: "Evolve gym location 5" },
+  { imageUrl: Slide6, alt: "Evolve gym location 6" },
+  { imageUrl: Slide7, alt: "Evolve gym location 7" },
+  { imageUrl: Slide8, alt: "Evolve gym location 8" },
+  { imageUrl: Slide9, alt: "Evolve gym location 9" },
+  { imageUrl: Slide10, alt: "Evolve gym location 10" },
+  { imageUrl: Slide11, alt: "Evolve gym location 11" },
+  { imageUrl: Slide12, alt: "Evolve gym location 12" },
+  { imageUrl: Slide13, alt: "Evolve gym location 13" },
+  { imageUrl: Slide14, alt: "Evolve gym location 14" },
+  { imageUrl: Slide15, alt: "Evolve gym location 15" },
+  { imageUrl: Slide16, alt: "Evolve gym location 16" },
+  { imageUrl: Slide17, alt: "Evolve gym location 17" },
+  { imageUrl: Slide18, alt: "Evolve gym location 18" },
+  { imageUrl: Slide19, alt: "Evolve gym location 19" },
+  { imageUrl: Slide20, alt: "Evolve gym location 20" }
+
+
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -94,8 +133,9 @@ const AccessEvolveLocationWithEase = () => {
       </div>
       </div>
 
+
       <Link to="https://join.evolvestrength.ca/tour-form/">
-            <button className="btnPrimary">BOOK A FREE TOUR</button>
+            <button className="btnPrimary mt-10 md:mt-0">BOOK A FREE TOUR</button>
             </Link>
 
     </div>
