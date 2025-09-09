9459f5923ff9fcee3a162279c3ed335ae71555a2
nomanqadir-cosmoinc <nomanqadir@cosmoinc.co>
2025-09-09 17:23:12 +0500

    wellness readabilty issue

--- src/components/PageComponents/Wellness/Desktop/WellnessPricingRoom.jsx
+++ src/components/PageComponents/Wellness/Desktop/WellnessPricingRoom.jsx
@@ -5,17 +5,17 @@ function WellnessPricingRoom() {
   return (
     <div className="relative WellnessPricingRoomBG">
       {/* <div className="absolute top-0 left-0 z-1 w-full h-full bg-black/40" /> */}
-      <div className="max-w-[1280px] px-4 md:px-8 pb-[80px] mx-auto w-full h-full">
+      <div className="max-w-[1280px] px-4 md:px-8 pb-[50px] md:pb-[80px] mx-auto w-full h-full">
         <div className="relative z-2 flex  flex-col items-start justify-end h-full">
           <h2 className="text-[#FFFFFF] uppercase max-w-[560px] !font-[700] leading-[39px] mb-5">
             Premium Wellness, Affordable Pricing
           </h2>
-          <p className="text-[#FFFFFF] description !font-[400] max-w-[542px] leading-[24px] mb-6">
+          <h4 className="text-[#FFFFFF] !font-[Vazirmatn] md:!font-[300] !font-[400] max-w-[552px] leading-[24px] mb-6">
             Our wellness services are open to everyone. You do not need to join
             the gym to book a session. Independent specialists set fair and
             transparent rates, so you get expert care at a price that makes
             sense.
-          </p>
+          </h4>
           <div className="flex gap-6">
             <Link to="/explore?category=wellness">
               <button className="btnPrimary">Find a wellness expert</button>
