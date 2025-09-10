52f21a1ad92bba58587728f9df5769ae3954d47a
Saif <saifali@cosmoinc.co>
2025-09-10 21:29:49 +0500

    Updated location urls

--- src/components/PageComponents/Locations/Desktop/AllLocations/AllGymLocations.jsx
+++ src/components/PageComponents/Locations/Desktop/AllLocations/AllGymLocations.jsx
@@ -12,7 +12,8 @@ function AllGymLocations() {
       address: "1920 Willingdon Ave #3105 Burnaby, British Columbia, V5C 0K3",
       phone: "+1 (236) 455-6573",
       directionsUrl:
-        "https://www.google.com/maps/place/Evolve+Strength+Brentwood/@49.267477,-123.0040773,[…]1r8z347_m?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
+      "https://www.google.com/maps/place/Evolve+Strength+Brentwood/@49.2674735,-123.0015024,736m/data=!3m3!1e3!4b1!5s0x548677304a5e7e31:0x4a38c21eac564f4f!4m6!3m5!1s0x548677502befb98d:0x80e13b4db43dc394!8m2!3d49.2674735!4d-123.0015024!16s%2Fg%2F11r8z347_m?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
+      // "https://www.google.com/maps/place/Evolve+Strength+Brentwood/@49.267477,-123.0040773,[…]1r8z347_m?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
       locationUrl: "/locations/burnaby-brentwood",
       commonbtn: false,
     },
@@ -24,7 +25,8 @@ function AllGymLocations() {
       address: "658 Homer St Vancouver, British Columbia, V6B 2R4",
       phone: "+1 (604) 555-0124",
       directionsUrl:
-        "https://www.google.com/maps/place/Evolve+Strength+Post/@49.2811894,-123.1159455,521m[…]1vqrh4g84?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
+        "https://www.google.com/maps/place/Evolve+Strength+Post/@49.2811859,-123.1133706,735m/data=!3m2!1e3!4b1!4m6!3m5!1s0x54867100126f4c37:0xcfb8565a259c5326!8m2!3d49.2811859!4d-123.1133706!16s%2Fg%2F11vqrh4g84?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
+        // "https://www.google.com/maps/place/Evolve+Strength+Post/@49.2811894,-123.1159455,521m[…]1vqrh4g84?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
       locationUrl: "/locations/vancouver-post",
       commonbtn: false,
     },
@@ -36,7 +38,8 @@ function AllGymLocations() {
       address: "710-19587 Seton Crescent SE Calgary, Alberta, T3M 2T5",
       phone: "+1 (825) 407-9015",
       directionsUrl:
-        "https://www.google.com/maps/place/Evolve+Strength+Seton/@50.8807854,-113.963425,504m[…]1nxr64bbp?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
+        "https://www.google.com/maps/place/Evolve+Strength+Seton/@50.880782,-113.9608501,711m/data=!3m2!1e3!4b1!4m6!3m5!1s0x53719d3715336be1:0x347c4581216ee053!8m2!3d50.880782!4d-113.9608501!16s%2Fg%2F11nxr64bbp?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
+        // "https://www.google.com/maps/place/Evolve+Strength+Seton/@50.8807854,-113.963425,504m[…]1nxr64bbp?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
       locationUrl: "/locations/calgary-seton",
       commonbtn: false,
     },
@@ -49,6 +52,7 @@ function AllGymLocations() {
       phone: "+1 (403) 452-3169",
       directionsUrl:
         "https://www.google.com/maps/place/Evolve+Strength+Royal+Oak/@51.1536466,-114.218384,707m/data=!3m2!1e3!4b1!4m6!3m5!1s0x537169b969aaab45:0x45bbd22c116daa40!8m2!3d51.1536433!4d-114.2158091!16s%2Fg%2F11pzrvbvhk?hl=en&entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
+        // "https://www.google.com/maps/place/Evolve+Strength+Royal+Oak/@51.1536466,-114.218384,707m/data=!3m2!1e3!4b1!4m6!3m5!1s0x537169b969aaab45:0x45bbd22c116daa40!8m2!3d51.1536433!4d-114.2158091!16s%2Fg%2F11pzrvbvhk?hl=en&entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
       locationUrl: "/locations/calgary-royal-oak",
       commonbtn: false,
     },
@@ -71,7 +75,8 @@ function AllGymLocations() {
       address: "12328 102 ave nw Edmonton, Alberta, T5N 0L9",
       phone: "+1 (780) 784-2675",
       directionsUrl:
-        "https://www.google.com/maps/place/evolve+strength+downtown/data=!4m2!3m1!1s0x53a0222b0e9db08f:0xce9a2cb44c32d720?sa=X&ved=1t:242&ictx=111",
+        "https://www.google.com/maps/place/Evolve+Strength+Downtown/@53.543337,-113.5354744,670m/data=!3m2!1e3!4b1!4m6!3m5!1s0x53a0222b0e9db08f:0xce9a2cb44c32d720!8m2!3d53.543337!4d-113.5354744!16s%2Fg%2F11f__756w3?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
+        // "https://www.google.com/maps/place/evolve+strength+downtown/data=!4m2!3m1!1s0x53a0222b0e9db08f:0xce9a2cb44c32d720?sa=X&ved=1t:242&ictx=111",
       locationUrl: "/locations/edmonton-downtown",
       commonbtn: false,
     },
@@ -83,7 +88,8 @@ function AllGymLocations() {
       address: "4825 89 St NW Edmonton, Alberta, T6E 5K1",
       phone: "+1 (780) 690-4252",
       directionsUrl:
-        "https://www.google.com/maps/place/Evolve+Strength+South/@53.4856256,-113.465725,475m[…]1b782yhvq?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
+        "https://www.google.com/maps/place/Evolve+Strength+South/@53.4856224,-113.4631501,671m/data=!3m3!1e3!4b1!5s0x53a018c417f73825:0xef1d3e6d01fde5e3!4m6!3m5!1s0x53a018c43ad69c97:0xdc7a5189de54d352!8m2!3d53.4856224!4d-113.4631501!16s%2Fg%2F11b782yhvq?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
+        // "https://www.google.com/maps/place/Evolve+Strength+South/@53.4856256,-113.465725,475m[…]1b782yhvq?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
       locationUrl: "/locations/edmonton-south",
       commonbtn: false,
     },
@@ -95,7 +101,8 @@ function AllGymLocations() {
       address: "13457 149 St Edmonton, Alberta, T5L 2T3",
       phone: "+1 (780) 784-7870",
       directionsUrl:
-        "https://www.google.com/maps/place/Evolve+Strength+North/@53.5958445,-113.5838443,474[…]1gdm5x66g?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
+        "https://www.google.com/maps/place/Evolve+Strength+North/@53.5958413,-113.5812694,669m/data=!3m3!1e3!4b1!5s0x53a0241c98bbcb79:0x79484bd406c1c77!4m6!3m5!1s0x53a0241c8a49c0db:0xf4ab68a7017116b!8m2!3d53.5958413!4d-113.5812694!16s%2Fg%2F11gdm5x66g?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
+        // "https://www.google.com/maps/place/Evolve+Strength+North/@53.5958445,-113.5838443,474[…]1gdm5x66g?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D",
       locationUrl: "/locations/edmonton-north",
       commonbtn: false,
     },
