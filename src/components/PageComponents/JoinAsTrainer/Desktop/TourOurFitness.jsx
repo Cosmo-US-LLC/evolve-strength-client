92ebe67e3c081f823b563039e95963caf0d833aa
faheem303 <faheemrehmat360@gmail.com>
2025-11-18 14:11:39 +0500

    mobile section feedback

--- src/components/PageComponents/JoinAsTrainer/Desktop/TourOurFitness.jsx
+++ src/components/PageComponents/JoinAsTrainer/Desktop/TourOurFitness.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 
 function TourOurFitness() {
   return (
-    <div className="relative TourOurFitness">
+
+    <div>
+    <div className="relative TourOurFitness max-md:hidden">
       <div className="absolute inset-0 bg-black/40 md:bg-black/20 z-0"></div>
       <div className="max-w-[1280px] px-4 pb-[40px] md:px-8 md:pb-[80px] mx-auto w-full h-full">
         <div className="relative z-2 flex  flex-col items-end justify-center h-full">
@@ -20,6 +22,28 @@ function TourOurFitness() {
           </div>
         </div>
       </div>
+    </div>
+<div className="relative md:hidden bg-[#000] ">
+      <div className="absolute inset-0 bg-black/40 md:bg-black/20 z-0"></div>
+      <div className="max-w-[1280px] px-4 pb-[40px] md:px-8 md:pb-[80px] mx-auto w-full h-full">
+        <div className="relative z-2 flex  flex-col items-end justify-center h-full">
+          <div>
+            <h2 className="text-[#FFFFFF] uppercase max-w-[560px] !font-[700] leading-[39px] mb-5">
+              KEEP WHAT YOUR EARN
+            </h2>
+            <h4 className="text-[#FFFFFF] !font-[400] md:!font-[300] max-w-[542px] leading-[24px] ">
+              Stop splitting your income and chasing quotas. 100% of what you
+              make is yours. You set your rates, choose your clients, and decide
+              how much you want to grow. Freedom isn’t just about time it’s
+              about financial control.
+            </h4>
+        </div>
+          </div>
+      </div>
+          <div className="min-h-[350px]">
+            <div className="TourOurFitness !w-[100%] min-h-[350px] left-0 abolute z-[9]"></div>
+          </div>
+    </div>
     </div>
   );
 }
