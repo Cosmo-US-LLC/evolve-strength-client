c331ee3901c907b46cf2732e07270651d5dadbeb
nomanqadir-cosmoinc <nomanqadir@cosmoinc.co>
2026-07-20 15:06:17 +0500

    updateh the home page copies

--- src/components/PageComponents/Home/Desktop/Hero.jsx
+++ src/components/PageComponents/Home/Desktop/Hero.jsx
@@ -5,7 +5,7 @@ function Hero({ title }) {
   return (
     <div>
       <div className="relative overflow-hidden w-full h-[70vh] md:h-[100vh] bg-[#000000]">
-         <video
+        <video
           autoPlay
           muted
           loop
@@ -34,28 +34,29 @@ function Hero({ title }) {
                   <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-white opacity-95"></span>
                   <span className="relative inline-flex rounded-full h-1.5 w-1.5 bg-white"></span>
                 </span>
-                New Location
+                Opening 2026
               </div>
               {/* <div className="text-white text-[12px] md:text-[14px] font-[400] uppercase leading-[24px] bg-[#20202066] backdrop-blur-[14px] border border-[#ADADAD] px-4 py-1 rounded-full font-[Kanit]">
                 OPENING MAY 25TH
               </div> */}
-
             </div>
-            <h1
-              className="text-[#FFFFFF] !text-[36px] md:!text-[70px] leading-[32px] md:leading-[59px] max-w-[740px] uppercase mb-4 md:mb-6"
-            >
-              South Edmonton,{" "}
-              <span className="text-[#4ab04a]">meet your new gym.</span>
+            <h1 className="text-[#FFFFFF] !text-[36px] md:!text-[70px] leading-[32px] md:leading-[59px] max-w-[740px] uppercase mb-4 md:mb-6">
+              Evolve Strength,{" "}
+              <span className="text-[#4ab04a]">Park Royal.</span>
             </h1>
-              {/* // dangerouslySetInnerHTML={{ __html: title }} */}
+            {/* // dangerouslySetInnerHTML={{ __html: title }} */}
 
             <h3 className="text-[#FFFFFF] !text-[16px] !font-[300] !leading-[20px] !md:leading-[24px] mb-8 max-w-[350px] md:max-w-[606px] !font-[Kanit]">
-              Book a free tour before July 1st for a chance to win a lifetime membership.
+              Join the waitlist to be first through the doors in West Vancouver.
             </h3>
             <div className="flex gap-2 md:gap-4 flex-col md:flex-row items-start">
-              <a href="https://evolvestrength.ca/tour-south-edmonton-common" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://evolvestrength.ca/park-royal-waitlist"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <button className="btnPrimary uppercase">
-                  Book A Free Tour
+                  Join the Waitlist
                 </button>
               </a>
             </div>
@@ -75,7 +76,6 @@ function Hero({ title }) {
 }
 
 export default Hero;
-
 
 // import React from "react";
 
